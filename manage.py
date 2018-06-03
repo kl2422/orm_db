8c676ebed009dc7c12ae5a14b52acb9bd06ed096
Tony <kl2422@163.com>
2018-06-03 21:34:05 +0800

    制造冲突

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    print(23324)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_db.settings")
     try:
         from django.core.management import execute_from_command_line
